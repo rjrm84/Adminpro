2b9151217e0687c018cd17d16e7fed79228b7d2a
Raul Rodriguez <rjrm84@gmail.com>
2018-09-24 23:31:27 -0300

    agregamos y modificamos modulos

---
+++ Desktop/Angular Avanzado/adminpro/src/app/pages/pages.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+import { PagesComponent } from './pages.component';
+
+
+// Modulos
+import { SharedModule } from '../shared/shared.module';
+
+
+// Rutas
+import { PAGES_ROUTING } from './pages.routes';
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    PagesComponent,
+  ],
+  exports: [DashboardComponent,
+     ProgressComponent,
+     Graficas1Component,
+      PagesComponent, ],
+  imports: [
+    SharedModule,
+    PAGES_ROUTING
+  ]
+
+})
+export class PagesModule {}
